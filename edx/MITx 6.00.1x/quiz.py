7208c3acea518339fed9e288c886561628ac34e8
jonolsu <jonathanbennett@yahoo.com>
2016-07-12 16:14:15 -0500

    completed quiz

--- edx/MITx 6.00.1x/quiz.py
+++ edx/MITx 6.00.1x/quiz.py
@@ -23,6 +23,54 @@ def flatten(aList):
     if isinstance(aList[0], list):
         return flatten(aList[0]) + flatten(aList[1:])
     return aList[:1] + flatten(aList[1:])
-            
 
-            
+def f(a,b):
+    return a+b
+
+def dict_interdiff(d1, d2):
+    '''
+    d1, d2: dicts whose keys and values are integers
+    Returns a tuple of dictionaries according to the instructions above
+    '''
+    intersect = list(set(d1.keys()) & set(d2.keys()))
+    intervalue = {}
+    for i in intersect:
+        intervalue[i] = f(d1[i],d2[i])
+
+    
+    diffvalue = {}
+    difference = list(set(d1.keys()).symmetric_difference(set(d2.keys())))
+    for d in difference:
+        if d in d1:
+            diffvalue[d] = d1[d]
+        else:
+            diffvalue[d] = d2[d]
+
+    return intervalue, diffvalue
+
+def f(s):
+    return 'a' in s
+        
+def satisfiesF(L):
+    """
+    Assumes L is a list of strings
+    Assume function f is already defined for you and it maps a string to a Boolean
+    Mutates L such that it contains all of the strings, s, originally in L such
+            that f(s) returns True, and no other elements. Remaining elements in L
+            should be in the same order.
+    Returns the length of L after mutation
+    """
+    newL = L[:]
+    for s in newL:
+        if not isinstance(s,str):
+            L.remove(s)
+        elif not (f(s)):
+            L.remove(s)
+    return (len(L))
+
+#run_satisfiesF(L, satisfiesF)
+      
+#L = ['a', 'b', 'a']
+#print satisfiesF(L)
+#print L
+
