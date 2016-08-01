d6a75ed3d950d5aa7a2b051fee2ed10dd351edfc
jonolsu <jonathanbennett@yahoo.com>
2016-08-01 08:22:42 -0500

    add learnpythonthehardway and pythonplayground

---
+++ learnpythonthehardway/README.md
@@ -0,0 +1,3 @@
+# learnpythonthehardway
+http://learnpythonthehardway.org/book/
+Using Python 3.4 to run a tutorial designed for 2.7
---
+++ learnpythonthehardway/ex11.py
@@ -0,0 +1,9 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print ("So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight))
+	
+	
+	C:\ProgramData\Oracle\Java\javapath;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\;C:\Program Files (x86)\Enterprise Vault\EVClient\;C:\Program Files\Intel\WiFi\bin\;C:\Program Files\Common Files\Intel\WirelessCommon\;c:\Program Files (x86)\Microsoft SQL Server\110\Tools\Binn\ManagementStudio\;c:\Program Files (x86)\Microsoft SQL Server\110\Tools\Binn\;c:\Program Files (x86)\Microsoft SQL Server\110\DTS\Binn\
